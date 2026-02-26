bc434aa9e526bc6febf6a8f46cc74b4924b07b54
Stanislav Pavliuk <stanislavpavlyk@gmail.com>
2026-02-26 03:21:01 +0200

    Extension structure

--- src/ldbc_extension.cpp
+++ src/ldbc_extension.cpp
@@ -3,43 +3,160 @@
 #include "ldbc_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
-#include "duckdb/function/scalar_function.hpp"
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
-
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
+#include "duckdb/function/pragma_function.hpp"
+#include "duckdb/function/table_function.hpp"
+#include "duckdb/main/extension/extension_loader.hpp"
 
 namespace duckdb {
 
-inline void LdbcScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Ldbc " + name.GetString() + " 🐥");
-	});
+struct SNBDatagenFunctionData : public TableFunctionData {
+	bool finished = false;
+	double sf = 0.003;
+	bool overwrite = false;
+	string catalog = INVALID_CATALOG;
+	string schema = DEFAULT_SCHEMA;
+	bool create_pg = false;
+};
+
+struct SNBListGlobalState : public GlobalTableFunctionState {
+	bool finished = false;
+};
+
+static unique_ptr<FunctionData> SNBDatagenBind(ClientContext &context, TableFunctionBindInput &input,
+                                               vector<LogicalType> &return_types, vector<string> &names) {
+	auto result = make_uniq<SNBDatagenFunctionData>();
+	for (auto &kv : input.named_parameters) {
+		if (kv.second.IsNull()) {
+			throw BinderException("Cannot use NULL as function argument");
+		}
+		if (kv.first == "sf") {
+			result->sf = kv.second.GetValue<double>();
+		} else if (kv.first == "overwrite") {
+			result->overwrite = kv.second.GetValue<bool>();
+		} else if (kv.first == "catalog") {
+			result->catalog = StringValue::Get(kv.second);
+		} else if (kv.first == "schema") {
+			result->schema = StringValue::Get(kv.second);
+		} else if (kv.first == "create_pg") {
+			result->create_pg = kv.second.GetValue<bool>();
+		}
+	}
+	if (input.binder) {
+		auto &catalog = Catalog::GetCatalog(context, result->catalog);
+		auto &properties = input.binder->GetStatementProperties();
+		properties.RegisterDBModify(catalog, context);
+	}
+
+	return_types.emplace_back(LogicalType::BOOLEAN);
+	names.emplace_back("success");
+	return std::move(result);
 }
 
-inline void LdbcOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Ldbc " + name.GetString() + ", my linked OpenSSL version is " +
-		                                           OPENSSL_VERSION_TEXT);
-	});
+static void SNBDatagenFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+	auto &data = data_p.bind_data->CastNoConst<SNBDatagenFunctionData>();
+	if (data.finished) {
+		return;
+	}
+	output.SetValue(0, 0, Value::BOOLEAN(true));
+	output.SetCardinality(1);
+	data.finished = true;
+}
+
+static unique_ptr<FunctionData> SNBQueriesBind(ClientContext &context, TableFunctionBindInput &input,
+                                               vector<LogicalType> &return_types, vector<string> &names) {
+	names.emplace_back("category");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("query_nr");
+	return_types.emplace_back(LogicalType::INTEGER);
+
+	names.emplace_back("query_pgq");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("query_sql");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("parameters");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("default_params");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	return nullptr;
+}
+
+static void SNBQueriesFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+	auto &state = data_p.global_state->Cast<SNBListGlobalState>();
+	if (state.finished) {
+		return;
+	}
+	state.finished = true;
+}
+
+static unique_ptr<FunctionData> SNBAnswersBind(ClientContext &context, TableFunctionBindInput &input,
+                                               vector<LogicalType> &return_types, vector<string> &names) {
+	names.emplace_back("category");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("query_nr");
+	return_types.emplace_back(LogicalType::INTEGER);
+
+	names.emplace_back("scale_factor");
+	return_types.emplace_back(LogicalType::DOUBLE);
+
+	names.emplace_back("answer");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	return nullptr;
+}
+
+static void SNBAnswersFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+	auto &state = data_p.global_state->Cast<SNBListGlobalState>();
+	if (state.finished) {
+		return;
+	}
+	state.finished = true;
+}
+
+static unique_ptr<GlobalTableFunctionState> SNBInit(ClientContext &context, TableFunctionInitInput &input) {
+	return make_uniq<SNBListGlobalState>();
+}
+
+static string PragmaSNBQuery(ClientContext &context, const FunctionParameters &parameters) {
+	auto category = StringValue::Get(parameters.values[0]);
+	if (category != "IC" && category != "IS" && category != "BI") {
+		throw InvalidInputException("Unsupported SNB category '%s'. Expected one of: IC, IS, BI", category);
+	}
+	auto query_nr = parameters.values[1].GetValue<int64_t>();
+	if (query_nr <= 0) {
+		throw InvalidInputException("SNB query number must be positive");
+	}
+	return "SELECT 'SNB query execution is not implemented yet' AS status";
 }
 
 static void LoadInternal(ExtensionLoader &loader) {
-	// Register a scalar function
-	auto ldbc_scalar_function = ScalarFunction("ldbc", {LogicalType::VARCHAR}, LogicalType::VARCHAR, LdbcScalarFun);
-	loader.RegisterFunction(ldbc_scalar_function);
+	TableFunction datagen_func("snb_datagen", {}, SNBDatagenFunction, SNBDatagenBind);
+	datagen_func.named_parameters["sf"] = LogicalType::DOUBLE;
+	datagen_func.named_parameters["overwrite"] = LogicalType::BOOLEAN;
+	datagen_func.named_parameters["catalog"] = LogicalType::VARCHAR;
+	datagen_func.named_parameters["schema"] = LogicalType::VARCHAR;
+	datagen_func.named_parameters["create_pg"] = LogicalType::BOOLEAN;
+	loader.RegisterFunction(datagen_func);
 
-	// Register another scalar function
-	auto ldbc_openssl_version_scalar_function = ScalarFunction("ldbc_openssl_version", {LogicalType::VARCHAR},
-	                                                            LogicalType::VARCHAR, LdbcOpenSSLVersionScalarFun);
-	loader.RegisterFunction(ldbc_openssl_version_scalar_function);
+	auto snb_pragma = PragmaFunction::PragmaCall("snb", PragmaSNBQuery, {LogicalType::VARCHAR, LogicalType::BIGINT});
+	loader.RegisterFunction(snb_pragma);
+
+	TableFunction queries_func("snb_queries", {}, SNBQueriesFunction, SNBQueriesBind, SNBInit);
+	loader.RegisterFunction(queries_func);
+
+	TableFunction answers_func("snb_answers", {}, SNBAnswersFunction, SNBAnswersBind, SNBInit);
+	loader.RegisterFunction(answers_func);
 }
 
 void LdbcExtension::Load(ExtensionLoader &loader) {
 	LoadInternal(loader);
 }
+
 std::string LdbcExtension::Name() {
 	return "ldbc";
 }
